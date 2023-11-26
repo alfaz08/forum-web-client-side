feebb0d676db41e6679d0c021c57abaa1b8575fc
Alfaz Hossain <alfaz.cse@gmail.com>
2023-11-26 10:18:17 +0600

    token set in user login

--- src/Providers/AuthProviders.jsx
+++ src/Providers/AuthProviders.jsx
@@ -1,7 +1,7 @@
 import { GoogleAuthProvider, createUserWithEmailAndPassword, onAuthStateChanged, signInWithEmailAndPassword, signInWithPopup, signOut, updateProfile } from "firebase/auth";
 import { createContext, useEffect, useState } from "react";
 import { auth } from "../firebase/firebase.init";
-
+import useAxiosPublic from "../hooks/useAxiosPublic" 
 
 export const AuthContext = createContext(null)
 
@@ -11,6 +11,8 @@ const AuthProviders = ({children}) => {
   const [user,setUser] =useState(null)
   const [loading,setLoading] =useState(true)
   const googleProvider = new GoogleAuthProvider()
+  const axiosPublic =useAxiosPublic()
+
 
   const googleSignIn =()=>{
     setLoading(true)
@@ -39,6 +41,18 @@ const AuthProviders = ({children}) => {
   useEffect(()=>{
     const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
       setUser(currentUser)
+      if(currentUser){
+        const userInfo ={email:currentUser.email}
+         axiosPublic.post('/jwt',userInfo)
+         .then(res=>{
+          if(res.data.token){
+            localStorage.setItem('access-token',res.data.token)
+          }
+         })
+      }
+      else{
+         localStorage.removeItem('access-token')
+      }
       setLoading(false)
     }); 
     
