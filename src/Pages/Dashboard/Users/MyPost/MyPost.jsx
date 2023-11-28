f78f393cf603a0d1cfccdbff320b1bd2a25764cf
Alfaz Hossain <alfaz.cse@gmail.com>
2023-11-28 12:57:07 +0600

    my post section all data show

--- src/Pages/Dashboard/Users/MyPost/MyPost.jsx
+++ src/Pages/Dashboard/Users/MyPost/MyPost.jsx
@@ -6,8 +6,45 @@ const MyPost = () => {
   console.log(myPost);
   return (
     <div>
-      <h2>My Post</h2>
+    <div className="flex justify-evenly my-4">
+    <h2>MyPost: {myPost.length}</h2>
+    
     </div>
+    <div className="overflow-x-auto">
+<table className="table table-zebra">
+  {/* head */}
+  <thead>
+    <tr>
+    <th>#</th>
+      <td>Title</td>
+      <td>Number of Votes</td>
+      <td>Make Admin</td>
+      <td>Subscription Status</td>
+    </tr>
+  </thead>
+  <tbody>
+   
+    
+   {
+    myPost.map((post,index)=>
+      <tr key={post._id}>
+      <th>{index+1}</th>
+      <td>{post.title}</td>
+      <td>{post.voteCount}</td>
+      <td>
+       <button className="btn btn-warning">All Comment</button>
+      </td>
+      <td>
+      <button className="btn btn-warning">Delete</button>
+      </td>
+    </tr>
+      )
+  }
+    
+  </tbody>
+</table>
+</div>
+  </div>
   );
 };
 
