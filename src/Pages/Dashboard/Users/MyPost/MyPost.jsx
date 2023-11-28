3d61802894dea49b18b70535ccebefee30db8195
Alfaz Hossain <alfaz.cse@gmail.com>
2023-11-28 14:14:51 +0600

    delete post from user end

--- src/Pages/Dashboard/Users/MyPost/MyPost.jsx
+++ src/Pages/Dashboard/Users/MyPost/MyPost.jsx
@@ -1,9 +1,40 @@
+import { Link } from "react-router-dom";
 import useMyPost from "../../../../hooks/useMyPost";
-
+import { FaTrashAlt } from "react-icons/fa";
+import Swal from "sweetalert2";
+import useAxiosSecure from "../../../../hooks/useAxiosSecure";
 
 const MyPost = () => {
-  const [myPost] = useMyPost()
+  const [myPost,refetch] = useMyPost()
   console.log(myPost);
+  const axiosSecure =useAxiosSecure()
+const handleDelete =(id)=>{
+  Swal.fire({
+    title: "Are you sure?",
+    text: "You won't be able to revert this!",
+    icon: "warning",
+    showCancelButton: true,
+    confirmButtonColor: "#3085d6",
+    cancelButtonColor: "#d33",
+    confirmButtonText: "Yes, delete it!"
+  }).then((result)=>{
+    if(result.isConfirmed){
+      axiosSecure.delete(`/posts/${id}`)
+      .then(res=>{
+        if(res.data.deletedCount>0){
+          refetch()
+          Swal.fire({
+            title:"Deleted",
+            text:"Your post has been deleted",
+            icon:"success"
+          })
+        }
+      })
+    }
+  })
+}
+
+
   return (
     <div>
     <div className="flex justify-evenly my-4">
@@ -32,10 +63,16 @@ const MyPost = () => {
       <td>{post.title}</td>
       <td>{post.voteCount}</td>
       <td>
-       <button className="btn btn-warning">All Comment</button>
+       <Link >
+       <button className="btn btn-warning">All Comment: </button>
+       </Link>
       </td>
       <td>
-      <button className="btn btn-warning">Delete</button>
+      <button 
+      onClick={()=>handleDelete(post._id)}
+      className="btn bg-warning ">
+        <FaTrashAlt className=""> </FaTrashAlt>
+      </button>
       </td>
     </tr>
       )
--- src/hooks/useMyPost.jsx
+++ src/hooks/useMyPost.jsx
@@ -8,14 +8,14 @@ import useAxiosSecure from "./useAxiosSecure";
 const useMyPost = () => {
   const axiosSecure =useAxiosSecure()
   const {user} =useAuth()
-  const {data: myPost=[]}=useQuery({
+  const {data: myPost=[],refetch}=useQuery({
     queryKey:['myPost',user?.email],
     queryFn:async()=>{
     const res =await axiosSecure.get(`/posts/my?email=${user?.email}`)
     return res.data
     }
   })
-  return [myPost]
+  return [myPost,refetch]
 };
 
 export default useMyPost;
